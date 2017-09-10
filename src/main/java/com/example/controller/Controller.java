0d881fcabf261e6b894a39f3625129ceaff7739d
nrkim <nrkim@administratorui-MacBook-Pro.local>
2017-09-10 22:35:58 +0900

    remove lombok

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,15 +1,17 @@
 package com.example.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
-@Slf4j
 @RestController
 public class Controller {
+
+    private static Logger logger = LoggerFactory.getLogger(Controller.class);
 
     //jmeter test용
     /*
@@ -20,8 +22,8 @@ public class Controller {
     @RequestMapping(value = "/fileupload", method = RequestMethod.POST)
     public String addFile(@RequestPart MultipartFile file){
         String originalFileName = file.getOriginalFilename();
-        log.info("this is add file. file name={} file type={}", originalFileName, file.getContentType());
-        log.info("new file path={}", this.getClass().getResource("/").toString());
+        logger.info("this is add file. file name={} file type={}", originalFileName, file.getContentType());
+        logger.info("new file path={}", this.getClass().getResource("/").toString());
 
         String dirPath = System.getProperty("user.dir") + "/build/temporary/";
         new File(dirPath).mkdir();
@@ -36,20 +38,20 @@ public class Controller {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public void testGet(){
-        log.info("this is test");
+        logger.info("this is test");
     }
 
     //jmeter random parameter
 //    random?gender=a
     @RequestMapping(value = "/random/{gender}", method = RequestMethod.GET)
     public boolean testRandomPathVariable(@PathVariable("gender") String gender){
-        log.info("testRandomPathVariable() {}", gender);
+        logger.info("testRandomPathVariable() {}", gender);
         return true;
     }
 
     @RequestMapping(value = "/random", method = RequestMethod.GET)
     public boolean testRandomRequestParam(@RequestParam String gender){
-        log.info("testRandomRequestParam() random requestParam {}", gender);
+        logger.info("testRandomRequestParam() random requestParam {}", gender);
         return true;
     }
 
--- src/test/java/com/example/controller/ControllerTest.java
+++ src/test/java/com/example/controller/ControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,25 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.io.InputStream;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ControllerTest{
 
     @Autowired
@@ -37,19 +30,16 @@ public class ControllerTest{
     @Before
     public void setUp() throws Exception {
 
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
     public void fileupload() throws Exception {
         // given
-//        MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt", "text/plain", "hello".getBytes());
         MockMultipartFile multipartFile = new MockMultipartFile("file", "logo.png", MediaType.MULTIPART_FORM_DATA_VALUE, "logo".getBytes());
 
         // when
         ResultActions result = this.mockMvc.perform(
-
                 fileUpload("/fileupload")
                         .file(multipartFile)
         ).andDo(print());
@@ -57,8 +47,4 @@ public class ControllerTest{
         // then
         result.andExpect(status().isOk());
     }
-
-
-
-
 }
